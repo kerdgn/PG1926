4da8ab5b2549f675c6a23de6547d3d65ea8df61e
kerdgn <79017965+kerdgn@users.noreply.github.com>
2021-02-13 20:26:14 +0300

    Create ProblemSet4.py

---
+++ ProblemSet4.py
@@ -0,0 +1,24 @@
+variable = None
+dizi = []
+x = 0
+
+while True:
+    variable = input("Lütfen veri giriniz. :")
+
+    try:
+        if variable == "c":
+            break
+        else:
+            dizi.append(int(variable))
+    except:
+        print('Lütfen sayı giriniz. :')
+
+
+print(format(dizi))
+
+for i in dizi :
+    if i % 2==1 and i>=x :
+        x=i
+
+print('En büyük tek sayı : ', x)
+© 2021 GitHub, Inc.
